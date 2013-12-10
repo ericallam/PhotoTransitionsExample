b3c3504c3b9053a3e9fdb46a29a0ce84ecbecec0
Eric Allam <rubymaverick@gmail.com>
2013-12-10 10:21:06 +0000

    Initial Commit

---
+++ PhotoTransitionsExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhotoTransitionsExample
+//
+//  Created by Eric Allam on 10/12/2013.
+//  Copyright (c) 2013 Code School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
